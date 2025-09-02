972b7e25b5e24557ce97d6b215448021d593890a
PraneshPK2005 <praneshvaradharaj@gmail.com>
2025-09-02 12:10:03 +0530

    updated signup.jsx

--- codes/frontend/src/pages/Signup.jsx
+++ codes/frontend/src/pages/Signup.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { API_BASE_URL } from '../config';
 
 export default function Signup() {
   const [username, setUsername] = useState('');
